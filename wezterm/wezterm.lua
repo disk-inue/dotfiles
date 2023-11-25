7d8602e741f6f14b4f210e03f4d7417c14479a6a
disk-inue <disk.inue.01@gmail.com>
2023-11-25 17:25:39 +0900

    feat: update wezterm and starship

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -14,7 +14,7 @@ end
 config.disable_default_key_bindings = true
 config.keys = require('keybinds').keys
 config.key_tables = require('keybinds').key_tables
-config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 2000 }
+config.leader = { key = "e", mods = "CTRL", timeout_milliseconds = 2000 }
 
 -- colors
 config.color_scheme = "Catppuccin Mocha"
