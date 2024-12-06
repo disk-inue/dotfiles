c25c780b73a5f01d6d86e2f4d4be21377f43e1b0
disk-inue <disk.inue.01@gmail.com>
2024-12-06 13:26:49 +0900

    fix : null-ls format

--- nvim/lua/extensions/none-ls.lua
+++ nvim/lua/extensions/none-ls.lua
@@ -1,17 +1,25 @@
+local lsp_formatting = function(bufnr)
+  vim.lsp.buf.format({
+    filter = function(client)
+      return client.name == "null-ls"
+    end,
+    bufnr = bufnr,
+  })
+end
+
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 require("null-ls").setup({
-  -- you can reuse a shared lspconfig on_attach callback here
   on_attach = function(client, bufnr)
     if client.supports_method("textDocument/formatting") then
       vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
       vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
         buffer = bufnr,
+        group = augroup,
         callback = function()
-          -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-          -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
-          vim.lsp.buf.format({ async = false })
+          lsp_formatting(bufnr)
         end,
+        desc = "[lsp] format on save",
       })
     end
   end,
