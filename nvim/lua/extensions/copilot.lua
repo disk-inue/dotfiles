fe76ae5546770d4419926532504d4edbbf9cb61e
disk-inue <disk.inue.01@gmail.com>
2025-04-30 10:02:24 +0900

    feat : copilot改善

--- nvim/lua/extensions/copilot.lua
+++ nvim/lua/extensions/copilot.lua
@@ -1,4 +1,20 @@
 require("copilot").setup({
   panel = { enabled = false },
-  suggestion = { enabled = false },
+  suggestion = { 
+    enabled = true,
+    auto_trigger = true,
+    debounce = 75,
+    keymap = {
+      accept = "<Tab>",
+      accept_word = false,
+      accept_line = false,
+      next = "<M-]>",
+      prev = "<M-[>",
+      dismiss = "<C-]>",
+    },
+  },
+  filetypes = {
+    ["*"] = true,
+  },
+  copilot_node_command = 'node',
 })
