909884975979d6cbdfdef973647e44bcd74d52bc
disk-inue <disk.inue.01@gmail.com>
2025-05-26 11:06:09 +0900

    chore : lsp周辺整理

--- nvim/lua/extensions/mason.lua
+++ nvim/lua/extensions/mason.lua
@@ -48,6 +48,105 @@ require("mason-lspconfig").setup({
     "marksman",      -- Markdown
     "ltex",          -- テキスト校正
   },
+
+  -- LSPがmasonでインストールされたあとの自動設定
+  handlers = {
+    function(server_name)
+      -- 各LSPサーバーの設定
+      local server_configs = {
+        -- Lua LSP
+        lua_ls = {
+          settings = {
+            Lua = {
+              diagnostics = {
+                globals = { "vim" }, -- Vimグローバル変数を認識
+              },
+              workspace = {
+                library = vim.api.nvim_get_runtime_file("", true), -- Neovim APIを認識
+                checkThirdParty = false,                           -- サードパーティチェックは無効化
+              },
+              telemetry = {
+                enable = false, -- テレメトリは無効化
+              },
+            },
+          },
+        },
+
+        -- TypeScript Language Server
+        ts_ls = {
+          settings = {
+            typescript = {
+              inlayHints = {
+                includeInlayParameterNameHints = "all",
+                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayVariableTypeHints = true,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayEnumMemberValueHints = true,
+              },
+            },
+            javascript = {
+              inlayHints = {
+                includeInlayParameterNameHints = "all",
+                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayVariableTypeHints = true,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayEnumMemberValueHints = true,
+              },
+            },
+            completions = {
+              completeFunctionCalls = true,
+            },
+          },
+        },
+
+        -- スタイリング
+        stylelint_lsp = {
+          filetypes = { "css", "scss", "less", "sass" },
+          settings = {
+            stylelintplus = {
+              autoFixOnFormat = true,
+            },
+          },
+        },
+
+        -- ESLint
+        eslint = {
+          on_attach = function(_, bufnr)
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              buffer = bufnr,
+              command = "EslintFixAll",
+            })
+          end,
+          settings = {
+            format = true, -- 保存時にフォーマット
+          },
+        },
+
+        -- 日本語校正LSP
+        ltex = {
+          settings = {
+            ltex = {
+              language = "ja",
+              additionalRules = { enablePickyRules = true },
+            },
+          },
+        },
+      }
+
+      -- サーバー固有の設定がある場合はそれを使用
+      local config = server_configs[server_name] or {}
+
+      -- 共通の設定を適用
+      config.capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      -- サーバーをセットアップ
+      require("lspconfig")[server_name].setup(config)
+    end,
+  },
 })
 
 -- LSPのケーパビリティを設定
